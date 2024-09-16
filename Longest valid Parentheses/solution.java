6e36cd18591552610c46b33add18f79d1e5b5785
Gaurav <gkp5625@gmail.com>
2024-09-16 20:09:09 +0530

    POTD 16th Sep

---
+++ Longest valid Parentheses/solution.java
@@ -0,0 +1,26 @@
+class Solution{
+    static int maxLength(String S){
+        // code here
+        Stack<Integer> stack = new Stack<>();
+        int ans =0;
+        int currlen=0;
+        stack.push(-1);
+        for(int i = 0;i<S.length();i++){
+            if(S.charAt(i)=='('){
+                stack.push(i);
+            }
+            else{
+                stack.pop();
+                if(stack.isEmpty()){
+                    stack.push(i);
+                }
+                else{
+                    currlen=i-stack.peek();
+                    ans=Math.max(ans,currlen);
+                }
+            }
+        }
+        return ans;
+            
+    }
+}
